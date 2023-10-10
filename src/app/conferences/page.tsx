a6e68849e51ad9a9957c6a92883cfb5f48e93093
Andrew <andrew.sideby@gmail.com>
2023-10-10 17:15:38 +0800

    variants /vearient/ is awesome!
    https://www.tailwind-variants.org/docs/getting-started

--- src/app/conferences/page.tsx
+++ src/app/conferences/page.tsx
@@ -1,9 +1,17 @@
-import { title } from '@/components/primitives';
+import { button, title } from '@/components/primitives';
+import { Button } from '@nextui-org/button';
 
 export default function PricingPage() {
+  console.error(button({ size: 'lg', color: 'warn' }));
+  console.error('=====');
+  console.error(button.defaultVariants);
   return (
     <div>
       <h1 className={title()}>Conferences</h1>
+
+      <Button radius="full" className={button()}>
+        @nextui-org/button{' '}
+      </Button>
     </div>
   );
 }
--- src/components/primitives.ts
+++ src/components/primitives.ts
@@ -1,53 +1,78 @@
-import { tv } from "tailwind-variants";
+import { tv } from 'tailwind-variants';
 
 export const title = tv({
-	base: "tracking-tight inline font-semibold",
-	variants: {
-		color: {
-			violet: "from-[#FF1CF7] to-[#b249f8]",
-			yellow: "from-[#FF705B] to-[#FFB457]",
-			blue: "from-[#5EA2EF] to-[#0072F5]",
-			cyan: "from-[#00b7fa] to-[#01cfea]",
-			green: "from-[#6FEE8D] to-[#17c964]",
-			pink: "from-[#FF72E1] to-[#F54C7A]",
-			foreground: "dark:from-[#FFFFFF] dark:to-[#4B4B4B]",
-		},
-		size: {
-			sm: "text-3xl lg:text-4xl",
-			md: "text-[2.3rem] lg:text-5xl leading-9",
-			lg: "text-4xl lg:text-6xl",
-		},
-		fullWidth: {
-			true: "w-full block",
-		},
-	},
-	defaultVariants: {
-		size: "md",
-	},
-	compoundVariants: [
-		{
-			color: [
-				"violet",
-				"yellow",
-				"blue",
-				"cyan",
-				"green",
-				"pink",
-				"foreground",
-			],
-			class: "bg-clip-text text-transparent bg-gradient-to-b",
-		},
-	],
+  base: 'tracking-tight inline font-semibold',
+  variants: {
+    color: {
+      violet: 'from-[#FF1CF7] to-[#b249f8]',
+      yellow: 'from-[#FF705B] to-[#FFB457]',
+      blue: 'from-[#5EA2EF] to-[#0072F5]',
+      cyan: 'from-[#00b7fa] to-[#01cfea]',
+      green: 'from-[#6FEE8D] to-[#17c964]',
+      pink: 'from-[#FF72E1] to-[#F54C7A]',
+      foreground: 'dark:from-[#FFFFFF] dark:to-[#4B4B4B]',
+    },
+    size: {
+      sm: 'text-3xl lg:text-4xl',
+      md: 'text-[2.3rem] lg:text-5xl leading-9',
+      lg: 'text-4xl lg:text-6xl',
+    },
+    fullWidth: {
+      true: 'w-full block',
+    },
+  },
+  defaultVariants: {
+    size: 'md',
+  },
+  compoundVariants: [
+    {
+      color: [
+        'violet',
+        'yellow',
+        'blue',
+        'cyan',
+        'green',
+        'pink',
+        'foreground',
+      ],
+      class: 'bg-clip-text text-transparent bg-gradient-to-b',
+    },
+  ],
 });
 
 export const subtitle = tv({
-	base: "w-full md:w-1/2 my-2 text-lg lg:text-xl text-default-600 block max-w-full",
-	variants: {
-		fullWidth: {
-			true: "!w-full",
-		},
-	},
-  defaultVariants:{
-    fullWidth: true
-  }
+  base: 'w-full md:w-1/2 my-2 text-lg lg:text-xl text-default-600 block max-w-full',
+  variants: {
+    fullWidth: {
+      true: '!w-full',
+    },
+  },
+  defaultVariants: {
+    fullWidth: true,
+  },
+});
+
+export const button = tv({
+  base: 'font-medium bg-blue-500 text-white rounded-full active:opacity-80',
+  variants: {
+    color: {
+      primary: 'bg-blue-500 text-white',
+      secondary: 'bg-pink-500 text-white',
+    },
+    size: {
+      sm: 'text-sm',
+      md: 'text-base',
+      lg: 'px-4 py-3 text-lg',
+    },
+  },
+  compoundVariants: [
+    {
+      size: ['sm', 'md'],
+      class: 'px-3 py-1',
+    },
+  ],
+  defaultVariants: {
+    size: 'md',
+    color: 'secondary',
+  },
 });
