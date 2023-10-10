1360927a411a36852d144ef298b90c7a7631ef68
Andrew <andrew.sideby@gmail.com>
2023-10-10 14:22:58 +0800

    nextUI v1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,32 @@
-import './globals.css'
-import type {Metadata} from 'next'
-import {Inter} from 'next/font/google'
-import React from "react";
-import TheHeader from "@/components/header";
-import TheFooter from "@/components/footer";
+import { Providers } from '@/app/providers';
+import TheFooter from '@/components/footer';
+import TheHeader from '@/components/header';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import React from 'react';
+import './globals.css';
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    title: 'Deban | Social Events',
-    description: 'Social Events With Pleasure!',
-}
+  title: 'Deban | Social Events',
+  description: 'Social Events With Pleasure!',
+};
 
 export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en" className="dark">
-        <body className={inter.className + " bg-gray-50 dark:bg-gray-900 text-white "}>
-        <TheHeader/>
-        {children}
-        <TheFooter/>
-        </body>
-        </html>
-    )
+  return (
+    <html lang="en" className="dark">
+      <body className={inter.className}>
+        <Providers>
+          <TheHeader />
+          {children}
+          <TheFooter />
+        </Providers>
+      </body>
+    </html>
+  );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,134 +1,135 @@
-import Image from 'next/image';
+'use client';
 
-export default function TheHeader() {
-    return (
-        <header
-            className="border-gray-200 bg-white dark:border-gray-600 sticky top-0 z-40 mx-auto w-full flex-none border-b">
-            <nav className="border-gray-200 bg-white dark:bg-gray-800 px-4 py-2 lg:px-6">
-                <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
-                    <a href="/" className="flex items-center">
-                        <Image
-                            src="/img/logo/logo.svg"
-                            className="mr-3 h-6 sm:h-9"
-                            alt=" Logo"
-                            width="36"
-                            height="36"
-                        />
-                        <span className="text-xl font-semibold dark:text-white self-center whitespace-nowrap">
-              SideBy
-            </span>
-                    </a>
+import DebanLogo from '@/components/icon/DebanLogo';
+import {
+  Avatar,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Input,
+  Link,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+} from '@nextui-org/react';
+import { SearchIcon } from '@nextui-org/shared-icons';
+import React from 'react';
 
-                    <div className="flex items-center lg:order-2">
-                        <a
-                            href="#"
-                            className="rounded-lg text-sm text-gray-800 hover:bg-gray-50 focus:ring-gray-300 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-800 mr-2 px-4 py-2 font-medium focus:outline-none focus:ring-4 lg:px-5 lg:py-2.5"
-                        >
-                            Log in
-                        </a>
-                        <div>
-                            <form>
-                                <select
-                                    id="countries"
-                                    className="rounded-lg border-gray-300 bg-gray-50 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:placeholder-gray-400 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:focus:border-blue-500 dark:focus:ring-blue-500 block w-full border p-2.5"
-                                >
-                                    <option key={1} defaultValue={'en'} value="en">
-                                        English
-                                    </option>
-                                    <option key={2} value="cn">
-                                        简体中文
-                                    </option>
-                                </select>
-                            </form>
-                        </div>
+export default function App() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-                        {/*--  a flex removed from button -*/}
-                        <button
-                            data-collapse-toggle="mobile-menu-2"
-                            type="button"
-                            className="rounded-lg text-sm text-gray-500 hover:bg-gray-100 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 ml-1 inline-flex items-center p-2 focus:outline-none focus:ring-2 lg:hidden"
-                            aria-controls="mobile-menu-2"
-                            aria-expanded="false"
-                        >
-                            <span className="sr-only">Open main menu</span>
-                            <svg
-                                className="h-6 w-6"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                    clipRule="evenodd"
-                                ></path>
-                            </svg>
-                            <svg
-                                className="hidden h-6 w-6"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                    clipRule="evenodd"
-                                ></path>
-                            </svg>
-                        </button>
-                    </div>
+  const menuItems = [
+    'Profile',
+    'Dashboard',
+    'Activity',
+    'Analytics',
+    'System',
+    'Deployments',
+    'My Settings',
+    'Team Settings',
+    'Help & Feedback',
+    'Log Out',
+  ];
 
-                    <div
-                        className="hidden w-full items-center justify-between lg:order-1 lg:flex lg:w-auto"
-                        id="mobile-menu-2"
-                    >
-                        <ul className="mt-4 flex flex-col font-medium lg:mt-0 lg:flex-row lg:space-x-8">
-                            <li>
-                                <a
-                                    href="/conferences"
-                                    className="rounded text-white bg-primary-700 dark:text-white lg:text-primary-700 block py-2 pl-3 pr-4 lg:bg-transparent lg:p-0"
-                                    aria-current="page"
-                                >
-                                    <p>Conferences</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="events"
-                                    className="border-gray-100 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white lg:hover:text-primary-700 lg:dark:hover:text-white block border-b py-2 pl-3 pr-4 lg:border-0 lg:p-0 lg:hover:bg-transparent lg:dark:hover:bg-transparent"
-                                    aria-current="page"
-                                >
-                                    <p>Events</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="spaces"
-                                    className="border-gray-100 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white lg:hover:text-primary-700 lg:dark:hover:text-white block border-b py-2 pl-3 pr-4 lg:border-0 lg:p-0 lg:hover:bg-transparent lg:dark:hover:bg-transparent"
-                                >
-                                    <p>Spaces</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="venues"
-                                    className="border-gray-100 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white lg:hover:text-primary-700 lg:dark:hover:text-white block border-b py-2 pl-3 pr-4 lg:border-0 lg:p-0 lg:hover:bg-transparent lg:dark:hover:bg-transparent"
-                                >
-                                    Venues
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="sharing"
-                                    className="border-gray-100 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white lg:hover:text-primary-700 lg:dark:hover:text-white block border-b py-2 pl-3 pr-4 lg:border-0 lg:p-0 lg:hover:bg-transparent lg:dark:hover:bg-transparent"
-                                >
-                                    Sharing
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
-    );
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen} isBordered>
+      <NavbarContent justify="start">
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+          className="sm:hidden"
+        />
+        <NavbarBrand className="mr-4">
+          <DebanLogo />
+          <p className="hidden font-bold text-inherit sm:block">ACME</p>
+        </NavbarBrand>
+        <NavbarContent className="hidden gap-3 sm:flex">
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              Features
+            </Link>
+          </NavbarItem>
+          <NavbarItem isActive>
+            <Link href="#" aria-current="page" color="success">
+              Customers
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              Integrations
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+      </NavbarContent>
+
+      <NavbarContent as="div" className="items-center" justify="end">
+        <Input
+          classNames={{
+            base: 'max-w-full sm:max-w-[10rem] lg:max-w-[20rem] h-10',
+            mainWrapper: 'h-full',
+            input: 'text-small',
+            inputWrapper:
+              'h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20',
+          }}
+          placeholder="Type to search..."
+          size="sm"
+          startContent={<SearchIcon width={24} />}
+          type="search"
+        />
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              color="secondary"
+              name="Jason Hughes"
+              size="sm"
+              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem key="profile" className="h-14 gap-2">
+              <p className="font-semibold">Signed in as</p>
+              <p className="font-semibold">zoey@example.com</p>
+            </DropdownItem>
+            <DropdownItem key="settings">My Settings</DropdownItem>
+            <DropdownItem key="team_settings">Team Settings</DropdownItem>
+            <DropdownItem key="analytics">Analytics</DropdownItem>
+            <DropdownItem key="system">System</DropdownItem>
+            <DropdownItem key="configurations">Configurations</DropdownItem>
+            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+            <DropdownItem key="logout" color="danger">
+              Log Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </NavbarContent>
+
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={
+                index === 2
+                  ? 'primary'
+                  : index === menuItems.length - 1
+                  ? 'danger'
+                  : 'foreground'
+              }
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
 }
