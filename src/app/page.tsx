8ed47c25a45316f5a93915776c03ceda493efbff
Andrew <andrew.sideby@gmail.com>
2023-10-11 11:40:16 +0800

    Merge NextUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,99 +1,79 @@
-import CardFlex from '@/components/cardFlex';
+'use client';
+
+import { Counter } from '@/components/counter';
+import { GithubIcon } from '@/components/icons';
+import { subtitle, title } from '@/components/primitives';
+import { siteConfig } from '@/config/site';
+import { conferencesList } from '@/utils/data';
+import { Link } from '@nextui-org/link';
+import { Card, CardBody, CardFooter, Divider, Image } from '@nextui-org/react';
+import { button as buttonStyles } from '@nextui-org/theme';
+import NextLink from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-      <section className="bg-white dark:bg-gray-900 bg-[url('/img/jumbotron/hero-pattern.svg')] dark:bg-[url('/img/jumbotron/hero-pattern-dark.svg')]">
-        <div className="relative z-10 mx-auto max-w-screen-xl px-4 py-8 text-center lg:py-10">
-          <a
-            href="https://cosmoverse.org/"
-            target="_blank"
-            className="rounded-full bg-blue-100 text-sm text-indigo-700 hover:bg-blue-200 dark:bg-indigo-900 dark:text-blue-300 dark:hover:bg-indigo-800 mb-7 inline-flex items-center justify-between px-1 py-1 pr-4"
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block  justify-center text-center">
+        <h1 className={title()}>
+          Social&nbsp;{' '}
+          <span className={title({ color: 'violet' })}>Events&nbsp;</span>
+          With Pleasure!
+        </h1>
+        <br />
+        <h1 className={title()}></h1>
+        <h2 className={subtitle({ class: 'mt-4' })}>
+          Epic conferences, side events, local meetups, webinars, venues rating,
+          hotel sharing...
+        </h2>
+      </div>
+
+      <div className="flex gap-3">
+        <Counter />
+
+        <Link
+          isExternal
+          as={NextLink}
+          className={buttonStyles({ variant: 'bordered', radius: 'full' })}
+          href={siteConfig.links.github}
+        >
+          <GithubIcon size={20} />
+          GitHub
+        </Link>
+      </div>
+
+      <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-3">
+        {conferencesList.map((item, index) => (
+          <Card
+            shadow="lg"
+            radius="sm"
+            key={index}
+            isPressable
+            onPress={() => console.log({ item })}
           >
-            <span className="rounded-full bg-indigo-600 text-xs text-white mr-3 px-4 py-1.5">
-              Upcoming
-            </span>
-            <span className="text-sm font-medium">Cosmmoverse at Istabbul</span>
-            <svg
-              className="ml-2 h-2.5 w-2.5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m1 9 4-4-4-4"
+            <CardBody className="overflow-visible p-0">
+              <Image
+                shadow="sm"
+                radius="sm"
+                width="100%"
+                alt={item.title}
+                className="object-fit mb- h-64 w-full"
+                src={item.cover}
               />
-            </svg>
-          </a>
-          <h1 className="text-2xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white sm:text-3xl md:text-5xl lg:text-6xl mb-4">
-            Social Events With Pleasure.
-          </h1>
-          <p className="text-lg font-normal text-gray-500 dark:text-gray-200 lg:text-xl mb-8 hidden sm:px-16 lg:block lg:px-48">
-            Epic conferences, side events, local meetups, webinars, venues
-            rating, hotel sharing... Well not quite anything, but all your need.
-          </p>
-          <form className="mx-auto w-full max-w-md">
-            <label
-              htmlFor="default-email"
-              className="text-sm text-gray-900 dark:text-white sr-only mb-2 font-medium"
-            >
-              Search Events
-            </label>
-            <div className="relative">
-              <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3.5">
-                <svg
-                  className="text-gray-500 dark:text-gray-400 h-4 w-4"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                  />
-                </svg>
+              <div>
+                <p className="mx-2 line-clamp-2">{item.title}</p>
+                <p className="mx-2 mt-2">{item.venue}</p>
+                <p className="mx-2 mt-2">
+                  {item.start_time}- {item.end_time}
+                </p>
               </div>
-              <input
-                type="input"
-                id="default-search"
-                className="rounded-lg border-gray-300 bg-white text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-700 dark:bg-gray-800 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 block w-full border p-4 pl-10"
-                placeholder="event keywords..."
-                required
-              />
-              <button
-                type="submit"
-                className="rounded-lg bg-blue-700 text-sm text-white hover:bg-blue-800 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 absolute bottom-2.5 right-2.5 px-4 py-2 font-medium focus:outline-none focus:ring-4"
-              >
-                Search
-              </button>
-            </div>
-          </form>
-        </div>
-        <div className="from-indigo-100 dark:from-gray-900 absolute left-0 top-0 z-0 h-full w-full bg-gradient-to-b to-transparent"></div>
-      </section>
-
-      <main className="relative">
-        <section className="bg-white dark:bg-gray-900">
-          <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-10">
-            <div className="mx-auto mb-2 text-center lg:mb-2">
-              <h3 className="text-2xl font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-3xl mb-4 text-left">
-                Epic Conferences
-              </h3>
-            </div>
-
-            <CardFlex />
-          </div>
-        </section>
-      </main>
-    </div>
+              <Divider />
+            </CardBody>
+            <CardFooter className="justify-between text-small">
+              <b>{item.host.name}</b>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </section>
   );
 }
