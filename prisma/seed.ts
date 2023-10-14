db3377fde385410df70a1ceebe313ebe4b259697
Andrew <andrew.sideby@gmail.com>
2023-10-14 09:30:50 +0800

    seed config

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -101,27 +101,23 @@ async function main() {
 
 const loadProduct = async () => {
   try {
-    await prisma.category.deleteMany();
-    console.log('Deleted records in category table');
-
-    await prisma.product.deleteMany();
-    console.log('Deleted records in product table');
-
+    // await prisma.category.deleteMany();
+    // console.log('Deleted records in category table');
+    // await prisma.product.deleteMany();
+    // console.log('Deleted records in product table');
     // await prisma.$queryRaw`ALTER TABLE Product AUTO_INCREMENT = 1`;
     // console.log('reset product auto increment to 1');
-
     // await prisma.$queryRaw`ALTER TABLE Category AUTO_INCREMENT = 1`;
     // console.log('reset category auto increment to 1');
-
-    await prisma.category.createMany({
-      data: categories,
-    });
-    console.log('Added category data');
-
-    await prisma.product.createMany({
-      data: products,
-    });
-    console.log('Added product data');
+    // await prisma.category.createMany({
+    //   data: categories,
+    // });
+    // console.log('Added category data');
+    //
+    // await prisma.product.createMany({
+    //   data: products,
+    // });
+    // console.log('Added product data');
   } catch (e) {
     console.error(e);
     process.exit(1);
